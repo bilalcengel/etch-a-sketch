42e6f06bcdb6c58ee15f4257c8f5cd6cc53fc465
bilal cengel <44057063+bilalcengel@users.noreply.github.com>
2024-06-05 17:01:33 +0300

    create grid,add button for setting number of squares.

---
+++ script.js
@@ -0,0 +1,29 @@
+let cont = document.querySelector("#container")
+let btn = document.querySelector("#btn")
+let x=0;
+btn.addEventListener('click',()=> {
+    x = prompt("enter number of squares per row")
+    if(x>100 || x<=0){
+        alert("invalid or too high value")
+    }else{
+        let child = cont.lastElementChild;
+        while (child) {
+            cont.removeChild(child);
+            child = cont.lastElementChild;
+        }
+        for (let i =0 ; i<x ; i++){
+            let grid1 = document.createElement("div");
+                grid1.classList.add("grid");
+                cont.appendChild(grid1);
+                for ( let j = 0 ; j < x ;j++){
+                    let grid2 = document.createElement("div");
+                    grid2.classList.add("grid1");
+                    grid2.addEventListener('mouseenter' ,() => { grid2.classList.add("black")});
+                    grid1.appendChild(grid2);
+                }
+        }
+    }
+
+});
+
+
